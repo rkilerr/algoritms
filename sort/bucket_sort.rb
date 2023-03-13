a044d4fea5718dd79cc9b408dcc4f8f0509b0c22
rkilerr <rvjobsupwork@gmail.com>
2023-03-13 08:18:22 -0700

    feat: add bucket sort

---
+++ sort/bucket_sort.rb
@@ -0,0 +1,39 @@
+require_relative "./quick_sort.rb"
+
+def bucket_sort(array, bucket_size = 5)
+  if array.empty? || array.length == 1
+    return array
+  end
+
+  min_value = array[0]
+  max_value = array[0]
+  array.each do |item|
+    if item < min_value
+      min_value = item
+    elsif item > max_value
+      max_value = item
+    end
+  end
+
+  bucket_count = ((max_value - min_value) / bucket_size).floor + 1
+  bucket_array = Array.new(bucket_count)
+
+  (0..bucket_array.length - 1).each do |index|
+    bucket_array[index] = []
+  end
+
+  array.each do |item|
+    bucket_array[((item - min_value) / bucket_size).floor] << item
+  end
+
+  sorted_array = []
+
+  bucket_array.each do |item|
+    new_arr = QuickSort.quick_sort(item, 0, item.length - 1)
+    sorted_array.concat(new_arr)
+  end
+
+  return sorted_array
+end
+
+puts bucket_sort([1, 30, 45, 29, 23, 34, 27])
--- sort/quick_sort.rb
+++ sort/quick_sort.rb
@@ -1,24 +1,26 @@
-def quick_sort(array, first, last)
-  if (first < last)
-    index = partiation(array, first, last)
-    quick_sort(array, first, index - 1)
-    quick_sort(array, index + 1, last)
-  end
-  return array
-end
-
-def partiation(array, first, last)
-  pivot = array[last]
-  p_index = first
-  i = first
-
-  while i < last
-    if (array[i].to_i <= pivot.to_i)
-      array[i], array[p_index] = array[p_index], array[i]
-      p_index += 1
+module QuickSort
+  def self.quick_sort(array, first, last)
+    if (first < last)
+      index = partiation(array, first, last)
+      quick_sort(array, first, index - 1)
+      quick_sort(array, index + 1, last)
     end
-    i +=1
+    return array
   end
-  array[p_index], array[last] = array[last], array[p_index]
-  return p_index
+
+  def self.partiation(array, first, last)
+    pivot = array[last]
+    p_index = first
+    i = first
+
+    while i < last
+      if (array[i].to_i <= pivot.to_i)
+        array[i], array[p_index] = array[p_index], array[i]
+        p_index += 1
+      end
+    i +=1
+    end
+    array[p_index], array[last] = array[last], array[p_index]
+    return p_index
+  end
 end
